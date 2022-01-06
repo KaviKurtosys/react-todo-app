ad362d0e1420fcd23e5a54546a8e66a5a80a7aa8
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2022-01-06 15:14:54 +0200

    Todo Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
-const Form = () => {
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value); 
+    };
+
+    const submitTodoHandle = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            { text: inputText, completed: false, id: (Math.random() * 1000) }
+        ])
+    };
+
     return (
         <form>
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type="submit">
+            <input 
+                value={inputText} 
+                onChange={ inputTextHandler } 
+                type="text" 
+                className="todo-input" 
+            />
+            <button onClick={ submitTodoHandle } className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
