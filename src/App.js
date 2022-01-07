6716ae59fe7fd64da154c80eb82343574ebfbd93
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2022-01-07 12:22:21 +0200

    Filtering List

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Form from './components/Form'; 
 import TodoList from './components/TodoList';
 
 function App() {
+ 
+
+  // State stuff
   const [ inputText, setInputText ] = useState('');
   const [ todos, setTodos ] = useState([]);
   const [ status, setStatus ] = useState('all');
-  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [ filteredTodos, setFilteredTodos] = useState([]);
+
+   // Use Effect
+   useEffect(() => {
+      filterHandler();
+    }, [todos, status]);
+
+  // Function
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true ));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false ));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  
   return (
     <div className="App">
       <header>
@@ -20,7 +44,7 @@ function App() {
         setInputText={ setInputText } 
         setStatus= { setStatus }
       />
-      <TodoList setTodos={ setTodos } todos={ todos }/>
+      <TodoList filteredTodos={ filteredTodos } setTodos={ setTodos } todos={ todos }/>
     </div>
   );
 }
